5cd4839615b4d3a662a526a8266c5bc8ddebe98e
abcsml <a338383154@163.com>
2023-06-23 15:45:59 +0800

    ai: add alpha-beta pruning

--- src/ai.rs
+++ src/ai.rs
@@ -8,7 +8,9 @@ pub trait AI<S> :Board<S> {
 }
 
 /// player指对谁来评分
-pub fn get_score<B: AI<S>,S: Step>(board: &B, step: S, me: Player, deep: u8) -> i16 {
+pub fn get_score<B: AI<S>,S: Step>(board: &B, step: S, me: Player,
+    deep: u8, alpha: i16, beta: i16) -> i16 {
+
     let player = step.who();
     let new_board = board.copy_put(step).unwrap();
 
@@ -19,9 +21,16 @@ pub fn get_score<B: AI<S>,S: Step>(board: &B, step: S, me: Player, deep: u8) -> 
     let mut max_score = i16::MIN;
     let mut min_score = i16::MAX;
     for s in new_board.get_possible_steps(player.rev()) {
-        let score = get_score(&new_board, s, me, deep-1);
+        let score = get_score(&new_board, s, me, deep-1, max_score, min_score);
         max_score = max(max_score, score);
         min_score = min(min_score, score);
+
+        // alpha，beta剪枝
+        if player == me && score < alpha {
+            break;
+        } else if player != me && score > beta {
+            break;
+        }
     }
     
     if player == me {
@@ -35,7 +44,7 @@ pub fn get_next_best_step<B: AI<S>,S: Step>(board: &B, player: Player) -> Option
     let mut best = None;
     let mut score = i16::MIN;
     for s in board.get_possible_steps(player) {
-        let ns = get_score(board, s, player, 30);
+        let ns = get_score(board, s, player, 30, i16::MIN, i16::MAX);
         // println!("step: {:?}: {}", s, ns);
         if score < ns {
             score = ns;
